835e239091fb8e9c2fc062d55c4de9901de0b1a7
Frederik Braun <fbraun+gh@mozilla.com>
2016-09-05 11:41:08 +0200

    actually start the schedule. d'oh.

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ var file = new nstatic.Server('html/', {
 function schedule() {
   // those exec calls are async
   //  but we simply don't care about the results because yolo
-  console.log(new Date(), "Requesting new screenshots.")
+  console.log(new Date(), "Requesting new screenshots.");
   exec('nodejs ./requestNewScreenshots.js');
 
   // we should ask for the URLs to the new screenshots... in two hours.
   setTimeout(function() {
-    console.log(new Date(), "Getting latest screenshot URLs.")
+    console.log(new Date(), "Getting latest screenshot URLs.");
     exec('nodejs ./getLatest.js', (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
@@ -39,6 +39,7 @@ function schedule() {
   // running again next week
   setTimeout(schedule, WEEK_MS)
 }
+schedule();
 
 // host stuff in html/
 const PORT = process.env.PORT || 4000;
